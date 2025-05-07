c004a59a5cfb232b43da42a8d994275b7f2c73fb
Jakub Tomiczek <tomiczekkuba01@gmail.com>
2025-05-07 15:35:56 +0200

    Initial Commit

---
+++ RickAndMortyApp/RickAndMortyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyAppApp.swift
+//  RickAndMortyApp
+//
+//  Created by Jakub Tomiczek on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
