ff5ef749f7208fa7284fd192a2af522b1434fded
Kuba Tomiczek <92593243+EddieCarbon@users.noreply.github.com>
2025-05-07 15:56:52 +0200

    Feature: Basic Architecture (#1)
    Adds the Composable Architecture (TCA) dependency to the project using Swift Package Manager.
    This change sets up the project to use TCA for managing application state, improving testability, and enabling more complex features.
    The RickAndMortyAppUITests target was removed.

---
+++ RickAndMortyApp/Features/CharacterDetails/CharacterDetailsReducer.swift
@@ -0,0 +1,10 @@
+//
+//  CharacterDetailsReducer.swift
+//  RickAndMortyApp
+//
+//  Created by Jakub Tomiczek on 07/05/2025.
+//
+
+import ComposableArchitecture
+import Foundation
+
---
+++ RickAndMortyApp/Features/EpisodeDetails/EpisodeDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  EpisodeDetailsView.swift
+//  RickAndMortyApp
+//
+//  Created by Jakub Tomiczek on 07/05/2025.
+//
+
+import SwiftUI
+
+struct EpisodeDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EpisodeDetailsView()
+}
